e9bc9d0bfd8e21fcb72f9557cc87f7e87b4becad
muckacid <moraga.inf@gmail.com>
2019-05-13 00:51:22 -0400

    add MVC Lab5 WCF .NET

---
+++ instituto/Segundo Año/Laboratotios.NET/Laboratorio5/Lab5/WCF/Modelo/LibroSt.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace WCF.Modelo
+{
+    [DataContract]
+    public class LibroSt
+    {
+        [DataMember(Name = "ID")]
+        public string IdLibro { get; set; }
+        [DataMember(Name = "Codigo")]
+        public string Codigo { get; set; }
+        [DataMember(Name = "Titulo")]
+        public string Titulo { get; set; }
+        [DataMember(Name = "ISBN")]
+        public string ISBN { get; set; }
+        [DataMember(Name = "Editorial")]
+        public string Editorial { get; set; }
+        [DataMember(Name = "Numero_de_Paginas")]
+        [DisplayName("Numero de Paginas")]
+        public string NumPags { get; set; }
+    }
+}
---
+++ instituto/Segundo Año/Laboratotios.NET/Laboratorio5/Lab5_MVC/WCF_MVC/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WCF_MVC.Controllers{
+    public class HomeController : Controller {
+        public ActionResult Index(){
+
+            return View();
+        }
+
+        public ActionResult ListaLibros(){
+
+            try
+            {
+                ServicioLibros.Service1Client conn = new ServicioLibros.Service1Client();
+                IEnumerable<WCF_MVC.ServicioLibros.LibroSt> lista = conn.GetLibrosBD();
+                return View(lista);
+            }
+            catch
+            {
+                return Redirect("Index");
+            }
+
+        }
+
+        public ActionResult CrearLibro(){
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CrearLibro(FormCollection FormCollection){
+
+            try{
+
+                ServicioLibros.Service1Client conn = new ServicioLibros.Service1Client();
+                ServicioLibros.LibroSt libro = new ServicioLibros.LibroSt();
+
+                libro.Codigo = FormCollection["Codigo"];
+                libro.Titulo = FormCollection["Titulo"];
+                libro.ISBN = FormCollection["ISBN"];
+                libro.Editorial = FormCollection["Editorial"];
+                libro.Numero_de_Paginas = FormCollection["Numero_de_Paginas"];
+
+                conn.SetLibroBD(libro);
+                return Redirect("ListaLibros");
+            }
+            catch{
+
+                return View();
+            }
+        }
+    }
+}
